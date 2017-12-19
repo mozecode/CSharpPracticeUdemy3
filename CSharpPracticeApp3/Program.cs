5430d3e17b7e287fce617a8374d7120be32c20f7
Greta Moseley <greta.moseley@gmail.com>
2017-12-19 12:25:00 -0600

    add Udemy practice app with C# ternary statement, data validation with TryParse

--- CSharpPracticeApp3/Program.cs
+++ CSharpPracticeApp3/Program.cs
@@ -9,7 +9,27 @@ namespace CSharpPracticeApp3
     class Program
     {
         static void Main(string[] args)
-        {
+        {//Write a program and ask the user to enter the width 
+         //and height of an image. Then tell if the image is landscape or portrait.
+
+            double height;
+            double width;
+
+            Console.WriteLine("Please enter the height of the image: ");
+            while (double.TryParse(Console.ReadLine(), out height) == false || height < 0)
+            {
+                Console.WriteLine("Sorry, try again.  Please enter the height of the image:");
+            }
+            
+            Console.WriteLine("Please enter the width of the image: ");
+            while (double.TryParse(Console.ReadLine(), out width) == false || width < 0)
+            {
+                Console.WriteLine("Sorry, try again.  Please enter the width of the image:");
+            }
+
+            var result = (width > height) ? "Landscape" : "Portrait";
+
+            Console.WriteLine("The picture is "+ result + " oriented.");
         }
     }
 }
